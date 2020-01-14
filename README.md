52297d5fcbb18c506e4e6aa28b91b5f923da6d86
madhur-taneja <tanejamadhur96@gmail.com>
2020-01-15 03:40:28 +0530

    Completing the project

--- README.md
+++ README.md
@@ -3,11 +3,16 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
+* [References](#references)
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+> The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.  
 
-To get started, open `js/app.js` and start building out the app's functionality
+> To get started, open `js/app.js` and start building out the app's functionality  
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+> For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## References
+
+* To check if an element is in Viewport or not from [vanillajstoolkit](https://vanillajstoolkit.com/helpers/isinviewport/) 
--- js/app.js
+++ js/app.js
@@ -19,6 +19,7 @@
 */
 
 let navbarList = document.querySelector("#navbar__list");
+const sections = document.querySelectorAll("section");
 
 /**
  * End Global Variables
@@ -26,7 +27,30 @@ let navbarList = document.querySelector("#navbar__list");
  * 
 */
 
+//Function to check if an element is in viewport or not
+var isInViewport = function (elem) {
+	var distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
 
+//Function to remove active classes
+function deactivateSections() {
+    sections.forEach((element)=>{
+        element.classList.remove("active");
+    });
+}
+
+function deactivateNavLinks() {
+    let navbarAnchors = document.querySelectorAll(".nav__hyperlink");
+    navbarAnchors.forEach((element)=>{
+        element.classList.remove("active-nav");
+    });
+}
 
 /**
  * End Helper Functions
@@ -35,17 +59,38 @@ let navbarList = document.querySelector("#navbar__list");
 */
 
 // build the nav
-window.addEventListener('load', buildNavbar() {
-    let listItem = document.createElement("li");
-	let sectionName = element.getAttribute("data-nav");
-	
-})
+window.addEventListener('load', buildNavbar())
 
 // Add class 'active' to section when near top of viewport
+function activateCurrentSection(currentSection) {
+    currentSection.classList.add("active");
 
+    deactivateNavLinks();
+    activateNavLinks(currentSection.getAttribute('id'));
+}
+
+function activateNavLinks(currentSectionId) {
+    let navbarAnchors = document.querySelectorAll(".nav__hyperlink");
+    //console.log(navbarAnchors);
+        navbarAnchors.forEach((element)=>{
+            if(element.getAttribute('href') == `#${currentSectionId}`) {
+                element.classList.add("active-nav");
+            }
+        });
+}
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollToSectionOnClick() {
+    let navbarAnchors = document.querySelectorAll(".nav__hyperlink");
+    navbarAnchors.forEach((element) => {
+        element.addEventListener("click", function(event) {
+            event.preventDefault();
+            document.querySelector(element.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
 
 /**
  * End Main Functions
@@ -53,10 +98,33 @@ window.addEventListener('load', buildNavbar() {
  * 
 */
 
-// Build menu 
+// Build menu
+function buildNavbar() {
+	sections.forEach((element)=>{
+	    let listItem = document.createElement("li");
+	    listItem.classList.add("navbar__list__item");
+    	let sectionName = element.getAttribute("data-nav");
+    	let currentSectionId = element.getAttribute("id");
+        listItem.innerHTML = `<a href="#${currentSectionId}" class="nav__hyperlink">${sectionName}</a>`;
+        navbarList.appendChild(listItem);
+    });
+}
 
 // Scroll to section on link click
+scrollToSectionOnClick();
 
 // Set sections as active
-
-
+window.addEventListener('scroll', function (event) {
+    sections.forEach((element)=>{
+        //console.log(element);
+        if (isInViewport(element)) {
+            deactivateSections();
+            activateCurrentSection(element);
+            //console.log('In viewport!');
+        } else if(window.scrollY==0) {
+            deactivateSections();
+            deactivateNavLinks();
+            //console.log('No Change');
+        }
+    }, false);
+});
